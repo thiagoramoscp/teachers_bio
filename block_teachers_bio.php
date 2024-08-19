09ff304dfc278fb1db6320e2a99303af4e952ddc
thiagoramoscp <thiagoramoscp@gmail.com>
2024-08-19 17:55:49 -0300

    fetch user information

--- block_teachers_bio.php
+++ block_teachers_bio.php
@@ -21,6 +21,10 @@
  * @copyright 2024 Thiago Ramos Cunha Pinheiro <thiagoramoscp@gmail.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+//require(__DIR__ . '/config.php');
+//require_once('../../config.php');
+
 class block_teachers_bio extends block_base {
 
     /**
@@ -40,23 +44,67 @@ class block_teachers_bio extends block_base {
     function get_content() {
         global $COURSE;
         global $USER;
+        global $DB;
+        global $OUTPUT;
 
         if ($this->content !== NULL) {
             return $this->content;
         }
 
         $this->content = new stdClass;
-        $this->content->footer = 'footer';
-        $this->content->text = $COURSE->id . ' ' . $USER->id . ' ' . $this->page->context->id;
 
-        //mdl_role_assignments
-        // select userid from mdl_role_assignments where contextid == $this->page->context->id and roleid == 3(professor)
+        $roleassignments = $DB->get_records('role_assignments',['contextid' => $this->page->context->id, 'roleid' => '3']);
+        $teachers = [];
+
+        $a = '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>/n<br><br><br>/n/n/n/n/n<br><br><br><br>';
+        var_dump($a);
+
+        for ($i = 0; $i < count($roleassignments); $i++) {
+            var_dump($i);
+            
+        }
+        var_dump($roleassignments);
+        foreach ($roleassignments as $roleassignment) {
+
+            //var_dump($roleassignment->contextid);
+            var_dump(count($roleassignments));
+            $teacherinfo = $DB->get_record('user',['id' => $roleassignment->userid]);
+            var_dump($teacherinfo);
+            
+            $teacher = new stdClass;
+            $teacher->contextid = $roleassignment->contextid;
+            
+            $teacher->id = $teacherinfo->id;
+            $teacher->picture = $teacherinfo->picture;
+            $teacher->imagealt = $teacherinfo->imagealt;
+            $teacher->firstname = $teacherinfo->firstname;
+            $teacher->firstnamephonetic = $teacherinfo->firstnamephonetic;
+            $teacher->lastnamephonetic = $teacherinfo->lastnamephonetic;
+            $teacher->middlename = $teacherinfo->middlename;
+            $teacher->alternatename = $teacherinfo->alternatename;
+            $teacher->lastname = $teacherinfo->lastname;
+            $teacher->email = $teacherinfo->email;
+            $teacher->profileimg = format_text($OUTPUT->user_picture($teacher), FORMAT_HTML);
+
+            array_push($teachers, $teacher);
+        }
+        var_dump($teachers);
+        
+        $c = format_text($OUTPUT->user_picture($teachers[0]), FORMAT_HTML);
+        var_dump($c);
+
+        $this->content->footer = $COURSE->id . ' ' . $USER->id . ' ' . $this->page->context->id;
+
+        
 
 
+        //$this->content->text = '';
 
-        //$data = ['YOUR DATA GOES HERE'];
+        $templatecontext = [
+            'teachers' => $teachers,
+        ];
 
-        //$this->content->text = $OUTPUT->render_from_template('block_teachers_bio/content', $data);
+        $this->content->text = $OUTPUT->render_from_template('block_teachers_bio/content', $templatecontext);
         
         return $this->content;
     }
